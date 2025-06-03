d7ad16b393f2349f3faf650d1fd45ad6e7917f3e
aksarv <119102043+aksarv@users.noreply.github.com>
2025-06-03 19:43:07 +0100

    Create helpers.py

---
+++ helpers.py
@@ -0,0 +1,53 @@
+"""
+NOT FINISHED
+"""
+
+import math
+import itertools
+
+pi = 3.1415926535897932384626433832795028851971693993751058209749445923078164062862
+e = 2.71828182845904523536028747135266249775724709369995
+
+
+def log(x, base):
+    return math.log(x, base)
+
+
+def sin(x):
+    return math.sin(x)
+
+
+def cos(x):
+    return math.cos(x)
+
+
+def tan(x):
+    return math.tan(x)
+
+
+def arcsin(x):
+    return math.asin(x)
+
+
+def arccos(x):
+    return math.acos(x)
+
+
+def arctan(x):
+    return math.atan(x)
+
+
+def abs(x):
+    return abs(x)
+
+
+def sinh(x):
+    return math.sinh(x)
+
+
+def cosh(x):
+    return math.cosh(x)
+
+
+def tanh(x):
+    return math.tanh(x)
